cb6a6d15b40cafa8975d284949e65586a600ac6f
BLisme <tranbaolam006@gmail.com>
2025-04-12 21:09:00 +0800

    can hover around the grid and change the background

--- js-file.js
+++ js-file.js
@@ -1,1 +1,30 @@
-console.log("HELLO!");
+let body=document.querySelector("body");
+let container =document.createElement("div")
+
+container.setAttribute("class", "container");
+
+container.style.display="flex";
+container.style.flexWrap="wrap";
+container.style.flex="1 1 100%";
+container.style.width="80%";
+container.style.justifyContent="space-between";
+
+for(let i =1;i<=256;i++){
+    let sq = document.createElement("div");
+    sq.style.border="1px solid black";
+    sq.style.flex="1 1 6%";
+    sq.style.height ="45px";
+
+    sq.addEventListener("mouseenter",function(e){
+        sq.style.backgroundColor="black";
+    });
+    sq.addEventListener("mouseleave",function(e){
+        sq.style.backgroundColor="white";
+    });
+
+
+
+    container.appendChild(sq);
+}
+
+body.appendChild(container);
